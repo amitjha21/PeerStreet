da905d9f04fd50c8b9f6a17c11e9aef2b7863726
amitjha <amitjha@usc.edu>
2018-05-17 00:33:53 -0700

    before editiin

--- ZipData/api/controller/nodeController.js
+++ ZipData/api/controller/nodeController.js
@@ -1,37 +1,42 @@
 'use strict';
 exports.read_a_task = function(req, res) {
     const fs = require('fs');
-    const path = './data/cbsa_to_msa.csv';
-    const rr = fs.createReadStream(path);
-    var a ;
-    // rr.on('readable', () => {
-    //     a = `${rr.read()}`;
-    //     console.log(a);
-    // });
-    // rr.on('end', () => {
-    //     console.log(a);
-    //     res.json(a);
-    // });
-
-
-    fs.readFile(path, 'utf8', function(err, data) {
-        if (err){
-            // console.log(err)
-            res.send(err);
-        }
-
-        // console.log('OK: ' + filename);
-        var dataLines = data.split("\n")
-        for (var i=0;i<10;i++){
-          console.log(dataLines[i])
-
-          console.log("NewLine1")
-        }
-        debugger;
-        console.log("Test");
-
-        //res.json(data);
+    const zip_to_cbsa = './data/zip_to_cbsa.csv';
+    const cbsa_to_msa = './data/cbsa_to_msa.csv';
+    const rr = fs.createReadStream(zip_to_cbsa);
+    var ZipCodeArr =req.query.zip ;
+    var a;
+    console.log(ZipCodeArr);
+    rr.on('readable', () => {
+        a = `${rr.read()}`;
+        console.log(a.length);
     });
+    rr.on('end', () => {
+        console.log(a);
+        res.json(a);
+    });
+
+
+    // fs.readFile(zip_to_cbsa, 'utf8', function(err, zipFileData) {
+    //     if (err){
+    //         console.log(err)
+    //         res.send(err);
+    //     }
+    //     console.log(zipFileData)
+    //     var zipFileDataLines = zipFileData.split("\n"),
+    //         zipCols = [];
+    //     //get Zip and CBSA
+    //
+    //     for (var i=1;i<zipFileDataLines.length;i++){
+    //       console.log(zipFileDataLines[i])
+    //       console.log("NewLine1")
+    //         zipCols.push(zipFileDataLines[i].split(","));
+    //
+    //     }
+    //     console.log(zipCols);
+    //
+    //     res.json(zipCols);
+    // });
 
     // fs.open('api/controller/file.txt', 'r', (err, fd) => {
     //     if (err) throw err;
--- test.rb
+++ test.rb
@@ -4,7 +4,11 @@ require 'json'
 
 #url = 'https://api.spotify.com/v1/search?type=artist&q=tycho'
 #url = 'https://jsonplaceholder.typicode.com/posts/1'
-url = 'http://localhost:3000/getPopData'
+zipCode = ["2001","1111","9999"]
+url = 'http://localhost:3000/getPopData?zip=' + zipCode.to_s
 uri = URI(url)
-response = Net::HTTP.get(uri)
-puts response
+response = Net::HTTP.get_response(uri)
+puts response.code       # => '200'
+puts response.message    # => 'OK'
+puts response.class.name
+puts response.body
